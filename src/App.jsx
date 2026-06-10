5e57f0233794477ab4ae3db9668f307da776d03c
vishnukant <vishnukantray10@gmail.com>
2026-06-10 14:35:37 +0530

    create route file

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import AppRoutes from "./routes/AppRoutes";
+// import { Toaster } from "react-hot-toast";
+
+function App() {
+  return (
+    <>
+      {/* <Toaster position="top-right" /> */}
+      <AppRoutes />
+    </>
+  );
+}
+
+export default App;
